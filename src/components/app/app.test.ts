ce234b83d85af87278b16a9cade1185e51b09822
Ryan <ryanlearyzafra@gmail.com>
2023-03-17 11:58:29 +0100

    Fix sonar and netlify bug

--- src/components/app/app.test.ts
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom";
-import { App } from "./App";
-App;
-describe("Given the ", () => {
-  test("Then it should ", () => {
-    expect(screen.getByRole("heading")).toBeInTheDocument();
-  });
-});
