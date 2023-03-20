2e9a1f82132c14f3401f0284beef60ac3e264401
Ryan <ryanlearyzafra@gmail.com>
2023-03-20 13:33:41 +0100

    Add user repo interface

---
+++ src/services/user.interface.ts
@@ -0,0 +1,6 @@
+import { ServerResponse, UserStructure } from "../models/user";
+
+export interface Repo<U> {
+  loginUser(info: Partial<UserStructure>): Promise<U>;
+  registerUser(info: UserStructure): Promise<U>;
+}
