5cc4200847ee271a6eb07d8046ba3c5aa905c3e4
Ryan <ryanlearyzafra@gmail.com>
2023-03-20 13:33:52 +0100

    Add user repo file

---
+++ src/services/users.api.repo.ts
@@ -0,0 +1,41 @@
+import { ServerResponse, UserStructure } from "../models/user";
+import { Repo } from "./user.interface";
+
+export class UserRepo implements Repo<ServerResponse> {
+  url?: string;
+  constructor() {
+    this.url = "http://localhost:4500/users";
+  }
+
+  async loginUser(user: Partial<UserStructure>): Promise<ServerResponse> {
+    const resp = await fetch(this.url + "/login", {
+      method: "POST",
+      body: JSON.stringify(user),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+
+    if (!resp.ok)
+      throw new Error(`Error http: ${resp.status} ${resp.statusText}`);
+
+    const data = (await resp.json()) as ServerResponse;
+    return data;
+  }
+
+  async registerUser(user: UserStructure): Promise<ServerResponse> {
+    const response = await fetch(this.url + "/register", {
+      method: "POST",
+      body: JSON.stringify(user),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+
+    if (!response.ok)
+      throw new Error(`Error http: ${response.status} ${response.statusText}`);
+
+    const data = await response.json();
+    return data;
+  }
+}
