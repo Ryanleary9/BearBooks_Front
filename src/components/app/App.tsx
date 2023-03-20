574f3a00ff89ce777eef56568401005789bf8f9f
Ryan <ryanlearyzafra@gmail.com>
2023-03-20 14:07:26 +0100

    Fix app file to test login component

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,7 +1,20 @@
+import Login from "../login/login";
+
+export type MenuOption = {
+  label: string;
+  path: string;
+};
+
+export const menuOptions: MenuOption[] = [
+  { label: "Home", path: "/home" },
+  { label: "Login", path: "/login" },
+  { label: "Register", path: "/register" },
+  { label: "Favorites", path: "/favorites" },
+];
 export default function App() {
   return (
     <div className="App">
-      <h1>Login</h1>
+      <Login></Login>
     </div>
   );
 }
