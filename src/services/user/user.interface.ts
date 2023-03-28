a489b9da04a42d3a83f6573d7354093db2169197
Ryan <ryanlearyzafra@gmail.com>
2023-03-28 10:46:23 +0200

    Fix user repo interface

--- src/services/user/user.interface.ts
+++ src/services/user/user.interface.ts
@@ -1,6 +1,10 @@
 import { UserStructure } from "../../models/user";
 
 export interface Repo<U> {
-  loginUser(info: Partial<UserStructure>): Promise<U>;
-  registerUser(info: UserStructure): Promise<U>;
+  create(user: UserStructure, path: string): Promise<U>;
+  kartActions(
+    kart: Partial<UserStructure>,
+    path: string,
+    mangaID: string
+  ): Promise<U>;
 }
