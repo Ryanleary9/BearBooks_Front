e7ccde48cdf1a0b9d903dfd99f66bf9002a0a50e
Ryan <ryanlearyzafra@gmail.com>
2023-03-29 13:19:53 +0200

    Add index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import App from "./components/app/App";
+import App from "./components/app/app";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 import { store } from "./store/store";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -12,7 +13,9 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>
 );
