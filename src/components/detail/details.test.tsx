63772bb5f7c260d146a468f15b3a85e457a124b3
Ryan <ryanlearyzafra@gmail.com>
2023-03-29 13:10:35 +0200

    Add details test file

---
+++ src/components/detail/details.test.tsx
@@ -0,0 +1,109 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { Manga } from "../../models/manga";
+import { UserStructure } from "../../models/user";
+import { mangaReducer } from "../../reducers/manga.slice";
+import { userReducer } from "../../reducers/slice";
+import { MangaRepo } from "../../services/manga/manga.api.repo";
+import { act, fireEvent, render, screen } from "@testing-library/react";
+import { useManga } from "../../hooks/manga/use.manga";
+import { Provider } from "react-redux";
+import { MemoryRouter as Router } from "react-router-dom";
+import Details from "./details";
+
+let paramsMock = { id: "1" };
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useParams: () => paramsMock,
+}));
+
+jest.mock("../../hooks/manga/use.manga");
+
+describe("Given the ", () => {
+  const repoMock = {
+    getAllMangas: jest.fn(),
+    getOneMangas: jest.fn(),
+    createMangas: jest.fn(),
+    updateMangas: jest.fn(),
+    deleteMangas: jest.fn(),
+  } as unknown as MangaRepo;
+
+  const storeMock = configureStore({
+    reducer: { mangas: mangaReducer, users: userReducer },
+    preloadedState: {
+      users: {
+        loggedUser: {} as UserStructure,
+        userLogged: {
+          email: "asdadasd",
+          passwd: "asdasdad",
+          role: "admin",
+          token: "asdadasd",
+        },
+        token: "",
+        users: [],
+      },
+      mangas: {
+        mangas: [
+          {
+            author: "kentaro",
+            category: "seinen",
+            description: "berserk desc",
+            firstChap: ["first chap"],
+            id: "1",
+            image: "image",
+            name: "Berserk",
+            price: 14,
+          },
+        ],
+        manga: {} as Manga,
+        mangaId: "1",
+      },
+    },
+  });
+
+  beforeEach(async () => {
+    (useManga as jest.Mock).mockReturnValue({
+      //adminState : {},
+      mangaState: {
+        mangas: [
+          {
+            author: "kentaro",
+            category: "seinen",
+            description: "berserk desc",
+            firstChap: ["first chap"],
+            id: "1",
+            image: "image",
+            name: "Berserk",
+            price: 14,
+          },
+        ],
+        mangaId: "1",
+      },
+      //getMangas: jest.fn(),
+      getMangaOne: jest.fn(),
+      mangaCreate: jest.fn(),
+      mangaUpdate: jest.fn(),
+      mangaDelete: jest.fn().mockImplementation(async () => Promise.resolve()),
+    });
+
+    await act(async () => {
+      render(
+        <Provider store={storeMock}>
+          <Router>
+            <Details></Details>
+          </Router>
+        </Provider>
+      );
+    });
+  });
+
+  test("Then it should ", async () => {
+    const data = await screen.findByAltText("Berserk");
+    expect(data).toBeInTheDocument();
+  });
+  test("Then it should ", async () => {
+    const data = await screen.findByText(/Delete/i);
+    await fireEvent.click(data);
+    expect(useManga(repoMock).mangaDelete).toHaveBeenCalled();
+  });
+});
