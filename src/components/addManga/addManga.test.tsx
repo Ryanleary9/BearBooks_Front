912b1d8c40ebd8eccff19351a0d86820cc89f62f
Ryan <ryanlearyzafra@gmail.com>
2023-03-29 13:18:57 +0200

    Add Add manga component test

---
+++ src/components/addManga/addManga.test.tsx
@@ -0,0 +1,63 @@
+import { screen, render } from "@testing-library/react";
+import Form from "../form/form";
+import { Provider } from "react-redux";
+import { store } from "../../store/store";
+import { MemoryRouter as Router } from "react-router-dom";
+import { Manga } from "../../models/manga";
+import { UserStructure } from "../../models/user";
+import { mangaReducer } from "../../reducers/manga.slice";
+import { userReducer } from "../../reducers/slice";
+import { configureStore } from "@reduxjs/toolkit";
+import AddManga from "./addManga";
+jest.mock("react-router-dom", () => ({
+  ...(jest.requireActual("react-router-dom") as any),
+  useNavigate: () => jest.fn().mockImplementation(() => ({})),
+}));
+describe("Given the ", () => {
+  const storeMock = configureStore({
+    reducer: { mangas: mangaReducer, users: userReducer },
+    preloadedState: {
+      users: {
+        loggedUser: {} as UserStructure,
+        userLogged: {
+          email: "asdadasd",
+          passwd: "asdasdad",
+          role: "admin",
+          token: "asdadasd",
+        },
+        token: "",
+        users: [],
+      },
+      mangas: {
+        mangas: [
+          {
+            author: "kentaro",
+            category: "seinen",
+            description: "berserk desc",
+            firstChap: ["first chap"],
+            id: "1",
+            image: "image",
+            name: "Berserk",
+            price: 14,
+          },
+        ],
+        manga: {} as Manga,
+        mangaId: "1",
+      },
+    },
+  });
+  test("Then it should ", () => {
+    render(
+      <Provider store={storeMock}>
+        <Router>
+          <AddManga>
+            <Form prop={true}></Form>
+          </AddManga>
+        </Router>
+      </Provider>
+    );
+
+    const page = screen.getByText("AddManga");
+    expect(page).toBeInTheDocument();
+  });
+});
