9368349605a089e1490d3586e3c8600544ad6fad
Ryan <ryanlearyzafra@gmail.com>
2023-03-29 13:02:37 +0200

    Add app component

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,20 +1,27 @@
-import Login from "../login/login";
+import { useSelector } from "react-redux";
+import { AppRouter } from "../app.router/app.router";
+import { Header } from "../header/header";
+import { Menu } from "../menu/menu";
+import "./app.scss";
+import { RootState } from "../../store/store";
 
 export type MenuOption = {
-  label: string;
+  label?: string;
   path: string;
 };
-
 export const menuOptions: MenuOption[] = [
   { label: "Home", path: "/home" },
   { label: "Login", path: "/login" },
   { label: "Register", path: "/register" },
-  { label: "Favorites", path: "/favorites" },
+  { label: "Add", path: "/add" },
 ];
 export default function App() {
   return (
-    <div className="App">
-      <Login></Login>
+    <div className="app">
+      <Header>
+        <Menu options={menuOptions}></Menu>
+      </Header>
+      <AppRouter menuOptions={menuOptions} />
     </div>
   );
 }
