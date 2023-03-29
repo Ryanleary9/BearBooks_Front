bb75879065a2fadbe89667557a6bde95c35e2e2e
Ryan <ryanlearyzafra@gmail.com>
2023-03-29 13:05:28 +0200

    Add app router file

---
+++ src/components/app.router/app.router.tsx
@@ -0,0 +1,56 @@
+import { Suspense, lazy } from "react";
+import { Routes, Route } from "react-router";
+import { MenuOption } from "../app/app";
+import Form from "../form/form";
+
+const LoginPage = lazy(() => import("../login/login"));
+const HomePage = lazy(() => import("../home/home"));
+const RegisterPage = lazy(() => import("../register/register"));
+const AddMangaPage = lazy(() => import("../addManga/addManga"));
+const EditMangaPage = lazy(() => import("../editManga/editManga"));
+const DetailsPage = lazy(() => import("../detail/details"));
+
+type AppRouterProps = {
+  menuOptions: MenuOption[];
+};
+export function AppRouter({ menuOptions }: AppRouterProps) {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path={"/"} element={<HomePage></HomePage>}></Route>
+        <Route
+          path={menuOptions[0].path}
+          element={<HomePage></HomePage>}
+        ></Route>
+        <Route
+          path={menuOptions[1].path}
+          element={<LoginPage></LoginPage>}
+        ></Route>
+        <Route
+          path={menuOptions[2].path}
+          element={<RegisterPage></RegisterPage>}
+        ></Route>
+        <Route
+          path={menuOptions[3].path}
+          element={
+            <AddMangaPage>
+              <Form prop={true}></Form>
+            </AddMangaPage>
+          }
+        ></Route>
+        <Route
+          path={"/edit/:id"}
+          element={
+            <EditMangaPage>
+              <Form prop={false}></Form>
+            </EditMangaPage>
+          }
+        ></Route>
+        <Route
+          path={"/detail/:id"}
+          element={<DetailsPage></DetailsPage>}
+        ></Route>
+      </Routes>
+    </Suspense>
+  );
+}
