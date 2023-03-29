bd504de7b1aea9c3e4828f5f437a6858952092b0
Ryan <ryanlearyzafra@gmail.com>
2023-03-29 13:19:13 +0200

    Add card component

---
+++ src/components/card/card.tsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import { Manga } from "../../models/manga";
+import "./card.scss";
+
+export type CardProps = {
+  manga: Manga;
+};
+
+export function Card({ manga }: CardProps) {
+  return (
+    <div className="card">
+      <li className="card-li">
+        <Link to={`/detail/${manga.id}`}>
+          <div className="Image container">
+            <img src={manga.image} alt="Manga's cover" className="card-image" />
+          </div>
+        </Link>
+
+        <p>{manga.name}</p>
+      </li>
+    </div>
+  );
+}
