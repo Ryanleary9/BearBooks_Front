667cdf99d2ad8bf1c34b28db9486889231d379e6
Ryan <ryanlearyzafra@gmail.com>
2023-03-29 12:25:57 +0200

    Add firebase config file

---
+++ src/firebase/firebase.config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDUBRJ8l9hab787v_GW2zhANVpcrOs5JSI",
+  authDomain: "bearbooks-482ff.firebaseapp.com",
+  projectId: "bearbooks-482ff",
+  storageBucket: "bearbooks-482ff.appspot.com",
+  messagingSenderId: "694397553561",
+  appId: "1:694397553561:web:865be38998290c3210a108",
+  measurementId: "G-X1L94PXW7Z",
+};
