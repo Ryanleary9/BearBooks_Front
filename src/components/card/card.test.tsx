2ca0557d2349ebe78d1d5d98a61ffe7c5f8a6e3d
Ryan <ryanlearyzafra@gmail.com>
2023-03-29 15:48:14 +0200

    Fix card test

---
+++ src/components/card/card.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react";
+import { Route, MemoryRouter as Router } from "react-router";
+import { Card } from "./card";
+describe("Given the ", () => {
+  const mockManga = {
+    author: "kentaro",
+    category: "seinen",
+    description: "berserk desc",
+    firstChap: ["first chap"],
+    id: "1",
+    image: "image",
+    name: "Berserk",
+    price: 14,
+  };
+
+  render(
+    <Router>
+      <Card manga={mockManga}></Card>
+    </Router>
+  );
+  describe("When ", () => {
+    test("Then it should ", async () => {
+      await screen.getByText("Berserk");
+    });
+  });
+});
