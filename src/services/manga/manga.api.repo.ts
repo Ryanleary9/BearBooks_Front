f5df61a31e2515b13debd8b3e0cf5023da672a5d
Ryan Leary <Ryanlearyzafra12@gmail.com>
2023-07-21 14:56:52 +0200

    Update manga.api.repo.ts url

--- src/services/manga/manga.api.repo.ts
+++ src/services/manga/manga.api.repo.ts
@@ -4,7 +4,7 @@ import { RepoManga } from "./manga.interface";
 export class MangaRepo implements RepoManga<MangaServerResponse> {
   url: string;
   constructor() {
-    this.url = "http://localhost:4500/manga";
+    this.url = "https://bearbooks-final-project.onrender.com/manga";
   }
 
   async getAllMangas(): Promise<MangaServerResponse> {
